57b841c3b88887bdf3a7fe49686b2db96fc7464c
jevvonn <darmawanb985@gmail.com>
2025-06-26 20:55:18 +0700

    fix: update password validation to remove alphanumeric requirement in RegisterRequest

--- internal/domain/dto/auth.go
+++ internal/domain/dto/auth.go
@@ -3,7 +3,7 @@ package dto
 type RegisterRequest struct {
 	Name     string `json:"name" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,alphanum,min=8,max=15"`
+	Password string `json:"password" validate:"required,min=8,max=15"`
 }
 
 type LoginRequest struct {
